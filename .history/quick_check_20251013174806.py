a51c04ec5e8f4e922e4f30a0786c84998e2171ca
jhimil-1 <jhimil@gmail.com>
2025-10-16 17:30:37 +0530

    yes

--- .history/quick_check_20251013174806.py
+++
@@ -1,8 +0,0 @@
-from database import MongoDB
-
-mongo = MongoDB()
-db = mongo.get_db()
-products = list(db.products.find({'category': 'electronics'}))
-print(f'Total electronics: {len(products)}')
-for i, p in enumerate(products):
-    print(f'{i+1}. {p["name"]}')
